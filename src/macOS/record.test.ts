73758a172df442f4df871c3d4f445e0549607c55
cmorten <craig.morten@hotmail.co.uk>
2022-09-18 15:38:51 +0100

    fix: stop recording via keypress to stdin

--- src/macOS/record.test.ts
+++ src/macOS/record.test.ts
@@ -1,40 +1,59 @@
+import { ChildProcess, execSync, spawn } from "child_process";
+import { join } from "path";
 import { mockType } from "../../test/mockType";
 import { record } from "./record";
-import { spawn } from "child_process";
+import { unlinkSync } from "fs";
 
 jest.mock("child_process", () => ({
+  execSync: jest.fn(),
   spawn: jest.fn(),
 }));
+jest.mock("fs", () => ({
+  unlinkSync: jest.fn(),
+}));
 
-const mockFilepath = "test-filepath";
+const mockDirectory = "test-directory";
+const mockFilepath = join(mockDirectory, "test-filepath.ext");
+
+const mockProcess = {
+  stdin: {
+    write: jest.fn(),
+  },
+};
 
 describe("record", () => {
-  let abortFunction;
+  let stopRecording;
 
   beforeEach(() => {
     jest.clearAllMocks();
 
-    abortFunction = record(mockFilepath);
+    mockType(spawn).mockReturnValue(mockProcess as any);
+
+    stopRecording = record(mockFilepath);
+  });
+
+  it("should create the directory path", () => {
+    expect(execSync).toHaveBeenCalledWith(`mkdir -p ${mockDirectory}`);
+  });
+
+  it("should delete the file if already exists (because the screencapture command won't overwrite)", () => {
+    expect(unlinkSync).toHaveBeenCalledWith(mockFilepath);
   });
 
   it("should spawn a screencapture child process for recording", () => {
-    expect(spawn).toHaveBeenCalledWith(
-      "/usr/sbin/screencapture",
-      ["-v", "-C", "-k", "-T0", "-g", mockFilepath],
-      {
-        signal: expect.any(AbortSignal),
-      }
-    );
+    expect(spawn).toHaveBeenCalledWith("/usr/sbin/screencapture", [
+      "-v",
+      "-C",
+      "-k",
+      "-T0",
+      "-g",
+      mockFilepath,
+    ]);
   });
 
-  it("should return an abort function to stop the screencapture process", () => {
-    const abortSignal = mockType(spawn).mock.calls[0][2].signal;
-    const mockReason = "test-reason";
+  it("should return a function to stop the screencapture process", () => {
+    stopRecording();
 
-    expect(abortSignal.aborted).toBeFalsy();
-
-    abortFunction(mockReason);
-
-    expect(abortSignal.aborted).toBeTruthy();
+    expect(mockProcess.stdin.write).toHaveBeenCalledWith("q");
   });
 });
--- src/macOS/record.ts
+++ src/macOS/record.ts
@@ -1,35 +1,27 @@
 import { execSync, spawn } from "child_process";
 import { dirname } from "path";
+import { unlinkSync } from "fs";
 
 /**
  * Start a screen recording.
  *
  * @param {string} filepath The file path to save the screen recording to.
- * @returns {Function} A function to abort the screen recording.
+ * @returns {Function} A function to stop the screen recording.
  */
-export function record(
-  filepath: string
-): typeof AbortController.prototype.abort {
+export function record(filepath: string): () => void {
   execSync(`mkdir -p ${dirname(filepath)}`);
+  unlinkSync(filepath);
 
-  const abortController = new AbortController();
+  const screencapture = spawn("/usr/sbin/screencapture", [
+    "-v",
+    "-C",
+    "-k",
+    "-T0",
+    "-g",
+    filepath,
+  ]);
 
-  const screencapture = spawn(
-    "/usr/sbin/screencapture",
-    ["-v", "-C", "-k", "-T0", "-g", filepath],
-    {
-      detached: true,
-      signal: abortController.signal,
-    }
-  );
-
-  screencapture.on("error", (err) => {
-    if (err.name === "AbortError") {
-      return;
-    }
-
-    throw err;
-  });
-
-  return () => abortController.abort();
+  return () => {
+    screencapture.stdin.write("q");
+  };
 }
