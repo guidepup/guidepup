4c1b771d27d7128a0411b104504186376eb72824
cmorten <craig.morten@hotmail.co.uk>
2022-12-18 15:22:47 +0000

    temp: add logging

--- src/windows/NVDA/getNVDAInstallationPath.ts
+++ src/windows/NVDA/getNVDAInstallationPath.ts
@@ -16,15 +16,21 @@ export async function getNVDAInstallationPath() {
     return null;
   }
 
+  console.log({ exists, values });
+
   const versions = Object.keys(values);
   const latestVersion = getMaxVersion(versions, ">=0");
   const guidepupNVDADirectory = values[latestVersion]?.value;
+
+  console.log({ versions, latestVersion, guidepupNVDADirectory });
 
   if (!guidepupNVDADirectory || !existsSync(guidepupNVDADirectory)) {
     return null;
   }
 
   const guidepupNVDAExecutablePath = join(guidepupNVDADirectory, "nvda.exe");
+
+  console.log({ guidepupNVDAExecutablePath });
 
   return (installationPath = guidepupNVDAExecutablePath);
 }
