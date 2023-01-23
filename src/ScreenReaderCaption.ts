ca2ee09765709af7396383763850f8e1b50f781a
cmorten <craig.morten@hotmail.co.uk>
2023-01-23 19:28:40 +0000

    fix: correct new ScreenReaderCaption interface

--- src/ScreenReaderCaption.ts
+++ src/ScreenReaderCaption.ts
@@ -1,21 +1,17 @@
-import type { CommandOptions } from "./CommandOptions";
-
 export interface ScreenReaderCaption {
   /**
    * Get the last spoken phrase.
    *
-   * @param {object} [options] Additional options.
    * @returns {Promise<string>} The last spoken phrase.
    */
-  lastSpokenPhrase(options?: CommandOptions): Promise<string>;
+  lastSpokenPhrase(): Promise<string>;
 
   /**
    * Get the text of the item in the ScreenReader cursor.
    *
-   * @param {object} [options] Additional options.
    * @returns {Promise<string>} The item's text.
    */
-  itemText(options?: CommandOptions): Promise<string>;
+  itemText(): Promise<string>;
 
   /**
    * Get the log of all spoken phrases for this ScreenReader instance.
