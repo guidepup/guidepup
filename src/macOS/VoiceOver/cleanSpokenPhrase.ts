2e7b05a0bdd735ba3270c9b90bc9f198bf58a4e7
cmorten <craig.morten@hotmail.co.uk>
2023-01-24 12:13:56 +0000

    feat: clean multiple whitespaces from vo output

--- src/macOS/VoiceOver/cleanSpokenPhrase.ts
+++ src/macOS/VoiceOver/cleanSpokenPhrase.ts
@@ -2,5 +2,6 @@ export function cleanSpokenPhrase(phrase: string): string {
   return phrase
     .trim()
     .replace(/^missing value$/, "")
-    .replaceAll("@VOModifier()", "Control-Option");
+    .replaceAll("@VOModifier()", "Control-Option")
+    .replace(/\s\s+/g, " ");
 }
