12ea27e761960e998c29c51d8e79b35ee3b186c0
Craig Morten <craig.morten@hotmail.co.uk>
2023-11-29 21:17:26 +0000

    test: don't allow playwright tests to infinite loop if failing

--- examples/playwright-voiceover/tests/headerNavigation.ts
+++ examples/playwright-voiceover/tests/headerNavigation.ts
@@ -7,6 +7,8 @@ async function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+const MAX_NAVIGATION_LOOP = 10;
+
 export async function headerNavigation({
   page,
   voiceOver,
@@ -27,13 +29,25 @@ export async function headerNavigation({
   // Make sure interacting with the web content
   await voiceOver.interact();
 
+  let headingCount = 0;
+
   // Move across the page menu to the Guidepup heading using VoiceOver 🔎
-  while ((await voiceOver.itemText()) !== "Guidepup heading level 1") {
+  while (
+    (await voiceOver.itemText()) !== "Guidepup heading level 1" ||
+    headingCount === MAX_NAVIGATION_LOOP
+  ) {
+    headingCount++;
     await voiceOver.perform(voiceOver.keyboardCommands.findNextHeading);
   }
 
+  let tabCount = 0;
+
   // Move through the README using standard keyboard commands
-  while ((await voiceOver.itemText()) !== "NVDA on Windows") {
+  while (
+    (await voiceOver.itemText()) !== "NVDA on Windows" ||
+    tabCount === MAX_NAVIGATION_LOOP
+  ) {
+    tabCount++;
     await voiceOver.press("Tab");
   }
 }
