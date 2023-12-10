2dbd1477da9fe9914edc58cd1a1c9ec976a3d375
cmorten <craig.morten@hotmail.co.uk>
2023-12-10 01:17:36 +0000

    ci: overcomplicated it?

--- examples/playwright-voiceover/voiceover-test.ts
+++ examples/playwright-voiceover/voiceover-test.ts
@@ -1,5 +1,6 @@
+import { macOSActivate, voiceOver } from "../../lib";
 import { test } from "@playwright/test";
-import { voiceOver } from "../../lib";
+import type { VoiceOver } from "../../lib";
 
 const applicationNameMap = {
   chromium: "Chromium",
@@ -18,47 +19,11 @@ const applicationNameMap = {
  *
  * A fresh started VoiceOver instance `vo` is provided to each test.
  */
-const voTest = test.extend<{ voiceOver: typeof voiceOver }>({
-  voiceOver: async ({ browserName, page }, use) => {
+const voTest = test.extend<{ voiceOver: VoiceOver }>({
+  voiceOver: async ({ browserName }, use) => {
     try {
-      const applicationName = applicationNameMap[browserName];
-
-      if (!applicationName) {
-        throw new Error(`Browser ${browserName} is not installed.`);
-      }
-
       await voiceOver.start();
-      await page.goto("about:blank", { waitUntil: "load" });
-
-      await voiceOver.perform(
-        voiceOver.keyboardCommands.openApplicationChooser
-      );
-
-      let applicationSwitchRetryCount = 0;
-
-      while (applicationSwitchRetryCount < 10) {
-        applicationSwitchRetryCount++;
-
-        await voiceOver.perform(voiceOver.keyboardCommands.moveDown);
-
-        const lastSpokenPhrase = await voiceOver.lastSpokenPhrase();
-
-        if (lastSpokenPhrase.includes(applicationName)) {
-          break;
-        }
-      }
-
-      await voiceOver.next();
-      await voiceOver.act();
-
-      // Force focus to somewhere in the web content
-      await page.locator("body").first().focus();
-      await voiceOver.perform(voiceOver.keyboardCommands.jumpToLeftEdge);
-
-      // Clear the log so clean for the actual test!
-      await voiceOver.clearSpokenPhraseLog();
-      await voiceOver.clearItemTextLog();
-
+      await macOSActivate(applicationNameMap[browserName]);
       await use(voiceOver);
     } finally {
       try {
