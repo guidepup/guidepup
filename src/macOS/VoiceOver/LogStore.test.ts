292197ef07f748d8257189888533284b7dd21ffa
cmorten <craig.morten@hotmail.co.uk>
2026-01-03 17:41:33 +0000

    test: additional test cases covering scenarios where VO never stabilizes

--- src/macOS/VoiceOver/LogStore.test.ts
+++ src/macOS/VoiceOver/LogStore.test.ts
@@ -2,6 +2,7 @@ import { cleanSpokenPhrase } from "./cleanSpokenPhrase";
 import { itemText } from "./itemText";
 import { lastSpokenPhrase } from "./lastSpokenPhrase";
 import { LogStore } from "./LogStore";
+import { MAX_SPOKEN_PHRASES_POLL_COUNT } from "./constants";
 
 jest.mock("./cleanSpokenPhrase", () => ({
   cleanSpokenPhrase: jest.fn(),
@@ -12,6 +13,7 @@ jest.mock("./constants", () => ({
   ITEM_TEXT_RETRY_COUNT: 1,
   SPOKEN_PHRASES_POLL_INTERVAL: 0,
   SPOKEN_PHRASES_RETRY_COUNT: 1,
+  MAX_SPOKEN_PHRASES_POLL_COUNT: 4,
 }));
 jest.mock("./itemText", () => ({
   itemText: jest.fn(),
@@ -60,7 +62,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with no capture options", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
@@ -115,7 +118,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with capture enabled", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
@@ -170,7 +174,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with capture disabled", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
@@ -216,6 +221,110 @@ describe("LogStore", () => {
         });
       });
     });
+
+    describe("when tap is called with capture enabled (by default or via options) but is unsuccessful in retrieving the last spoken phrase", () => {
+      const expectedResult = Symbol("test-expected-result");
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
+
+      beforeEach(() => {
+        jest
+          .mocked(lastSpokenPhrase)
+          .mockRejectedValue(new Error("test-error"));
+        jest.mocked(cleanSpokenPhrase).mockReturnValue("");
+
+        const action = () =>
+          new Promise((resolve) => {
+            resolver = resolve;
+          });
+
+        resultPromise = logStore.tap(action);
+      });
+
+      afterEach(async () => {
+        resolver(expectedResult);
+        await resultPromise;
+      });
+
+      describe("when the action completes", () => {
+        beforeEach(() => {
+          resolver(expectedResult);
+        });
+
+        it("should attempt to retrieve the log a maximum number of times specified by `MAX_SPOKEN_PHRASES_POLL_COUNT`", async () => {
+          await resultPromise;
+
+          expect(lastSpokenPhrase).toHaveBeenCalledTimes(
+            MAX_SPOKEN_PHRASES_POLL_COUNT
+          );
+        });
+
+        it("should not log any additional phrases", async () => {
+          expect(await logStore.spokenPhraseLog()).toEqual([""]);
+
+          await resultPromise;
+
+          expect(await logStore.spokenPhraseLog()).toEqual([""]);
+        });
+
+        it("should return an empty string", async () => {
+          expect(await logStore.lastSpokenPhrase()).toEqual("");
+        });
+      });
+    });
+
+    describe("when tap is called with capture enabled (by default or via options) but the phrases emitted by VO never stabilize (e.g. live region that announces updates every second)", () => {
+      const expectedResult = Symbol("test-expected-result");
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
+
+      beforeEach(() => {
+        let phraseCount = 0;
+        jest.mocked(lastSpokenPhrase).mockImplementation(() => {
+          return Promise.resolve(`${phraseCount++}`);
+        });
+
+        const action = () =>
+          new Promise((resolve) => {
+            resolver = resolve;
+          });
+
+        resultPromise = logStore.tap(action);
+      });
+
+      afterEach(async () => {
+        resolver(expectedResult);
+        await resultPromise;
+      });
+
+      describe("when the action completes", () => {
+        beforeEach(() => {
+          resolver(expectedResult);
+        });
+
+        it("should attempt to retrieve the log a maximum number of times specified by `MAX_SPOKEN_PHRASES_POLL_COUNT`", async () => {
+          await resultPromise;
+
+          expect(lastSpokenPhrase).toHaveBeenCalledTimes(
+            MAX_SPOKEN_PHRASES_POLL_COUNT
+          );
+        });
+
+        it("should log the combined last spoken phrases", async () => {
+          await resultPromise;
+
+          expect(await logStore.spokenPhraseLog()).toEqual([
+            `cleaned_0. cleaned_1. cleaned_2. cleaned_3`,
+          ]);
+        });
+
+        it("should return the combined last spoken phrases from the last action", async () => {
+          expect(await logStore.lastSpokenPhrase()).toEqual(
+            `cleaned_0. cleaned_1. cleaned_2. cleaned_3`
+          );
+        });
+      });
+    });
   });
 
   describe("when capture has been enabled by default", () => {
@@ -245,7 +354,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with no capture options", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
@@ -300,7 +410,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with capture enabled", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
@@ -383,7 +494,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with capture disabled", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
@@ -458,7 +570,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with no capture options", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
@@ -507,7 +620,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with capture enabled", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
@@ -562,7 +676,8 @@ describe("LogStore", () => {
 
     describe("when tap is called on an action with capture disabled", () => {
       const expectedResult = Symbol("test-expected-result");
-      let resultPromise, resolver;
+      let resultPromise: Promise<unknown>,
+        resolver: (r: typeof expectedResult) => void;
 
       beforeEach(() => {
         const action = () =>
