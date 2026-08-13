7a92d164165321639b6cfa524dc24399f6a49487
cmorten <craig.morten@hotmail.co.uk>
2026-08-13 20:54:03 +0100

    feat: placeholder start and stop

---
+++ examples/playwright-orca/tests/firefox/firefox.spec.ts
@@ -0,0 +1,52 @@
+import { platform, release } from "os";
+import { headerNavigation } from "../headerNavigation";
+import itemTextSnapshot from "./firefox.itemText.snapshot.json";
+import { logIncludesExpectedPhrases } from "../../../logIncludesExpectedPhrases";
+import spokenPhraseSnapshot from "./firefox.spokenPhrase.snapshot.json";
+import { orcaTest as test } from "../../orca-test";
+
+test.use({
+  orcaStartOptions: {
+    capture: "initial",
+  },
+});
+
+test.describe("Firefox Playwright Orca", () => {
+  test("I can navigate the Guidepup Github page", async ({
+    browser,
+    browserName,
+    page,
+    orca,
+  }) => {
+    const osName = platform();
+    const osVersion = release();
+    const browserVersion = browser.version();
+    const screenReaderName = orca.name;
+    const screenReaderVersion = orca.version;
+    const { retry } = test.info();
+
+    console.table({
+      osName,
+      osVersion,
+      browserName,
+      browserVersion,
+      screenReaderName,
+      screenReaderVersion,
+      retry,
+    });
+
+    await headerNavigation({ page, orca });
+
+    // Assert that we've ended up where we expected and what we were told on
+    // the way there is as expected.
+
+    const itemTextLog = await orca.itemTextLog();
+    const spokenPhraseLog = await orca.spokenPhraseLog();
+
+    console.log(JSON.stringify(itemTextLog, undefined, 2));
+    console.log(JSON.stringify(spokenPhraseLog, undefined, 2));
+
+    logIncludesExpectedPhrases(itemTextLog, itemTextSnapshot);
+    logIncludesExpectedPhrases(spokenPhraseLog, spokenPhraseSnapshot);
+  });
+});
