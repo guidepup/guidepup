26b92d48c5cd24ab2a518edfb220e43a9575290e
cmorten <craig.morten@hotmail.co.uk>
2026-08-21 22:43:54 +0100

    feat: wait for dbus success signals before assume is running

--- src/linux/Orca/isRunning.ts
+++ src/linux/Orca/isRunning.ts
@@ -1,8 +1,13 @@
 import { base } from "../../debug";
 import { execFile } from "child_process";
 import { promisify } from "util";
+import { sessionBus } from "dbus-native";
 
 const debug = base.extend("isRunning");
+
+const SERVICE = "org.gnome.Orca.Service";
+const OBJECT_PATH = "/org/gnome/Orca/Service/SystemInformationPresenter";
+const INTERFACE = "org.gnome.Orca.Module";
 
 const execFileAsync = promisify(execFile);
 
@@ -20,7 +25,16 @@ export async function isRunning(): Promise<boolean> {
 
     debug("`orca --list-apps`: ", apps);
 
-    return apps.includes("orca");
+    if (!apps.includes("orca")) {
+      return false;
+    }
+
+    const bus = sessionBus();
+    await bus.getInterface(SERVICE, OBJECT_PATH, INTERFACE);
+    debug(`D-Bus service "${SERVICE}" is available`);
+    await bus.close();
+
+    return true;
   } catch (cause) {
     debug("checks failed: ", cause);
 
