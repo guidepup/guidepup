6f725c168b6d0130cfa1c6d4133d08514f7eb566
cmorten <craig.morten@hotmail.co.uk>
2026-09-01 11:23:34 +0100

    fix: run from dir that exists?

--- src/linux/Orca/OrcaClient.ts
+++ src/linux/Orca/OrcaClient.ts
@@ -302,7 +302,7 @@ export class OrcaClient {
       {
         env: {
           ...process.env,
-          GUIDEPUP_ORCA_SPEECH_SOCKET: `${SPEECHD_DIR}/out/guidepup-orca-speech.sock`,
+          GUIDEPUP_ORCA_SPEECH_SOCKET: `${SPEECHD_DIR}/run/guidepup-orca-speech.sock`,
         },
       },
     );
