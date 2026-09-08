391693d863405d476835dbaedb79955c2407b07b
cmorten <craig.morten@hotmail.co.uk>
2026-09-08 18:14:40 +0100

    tmp: debug perform() command error

--- src/linux/Orca/Orca.ts
+++ src/linux/Orca/Orca.ts
@@ -14,6 +14,7 @@ import {
   setSettings,
 } from "./settings";
 import { type KeyCodeCommand, keyCodeCommands } from "./keyCodeCommands";
+import { base } from "../../debug";
 import type { Capture } from "../../Capture";
 import type { ClickOptions } from "../../ClickOptions";
 import type { CommandOptions } from "../../CommandOptions";
@@ -24,6 +25,8 @@ import { notImplemented } from "../../notImplemented";
 import { OrcaClient } from "./OrcaClient";
 import type { Prettify } from "../../typeHelpers";
 import type { StartOptions } from "../../StartOptions";
+
+const debug = base.extend("Orca");
 
 type CaptureCommandOptions = Prettify<Pick<CommandOptions, "capture">>;
 type CaptureStartOptions = Prettify<Pick<StartOptions, "capture" | "settings">>;
@@ -733,9 +736,19 @@ export class Orca implements IScreenReader {
     }
 
     await this.#client.enqueueAndTap(async () => {
-      await this.#client.service[command.service].commands[
-        command.command
-      ].execute();
+      const service = this.#client.service[command.service];
+      const commandDefinition = service?.commands[command.command];
+
+      debug({
+        command,
+        service: command.service,
+        commandName: command.command,
+        availableServices: Object.keys(this.#client.service),
+        availableCommands: service ? Object.keys(service.commands) : [],
+        commandDefinition,
+      });
+
+      await commandDefinition.execute();
     }, options);
   }
 
